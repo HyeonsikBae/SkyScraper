203d4dd7642ab67cc013e54a28b5f05bf197f80d
Hyeonsik Bae <hybae@c1r8s1.42seoul.kr>
2020-07-12 20:25:27 +0900

    check norm error in backtracking.c

--- src/backtracking.c
+++ src/backtracking.c
@@ -6,7 +6,7 @@
 /*   By: hybae <gustlr0217@naver.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/12 17:10:01 by hybae             #+#    #+#             */
-/*   Updated: 2020/07/12 18:38:33 by hybae            ###   ########.fr       */
+/*   Updated: 2020/07/12 20:23:17 by hybae            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,50 +22,49 @@ int		backtracking(int **board, int **answer, int size)
 	int	*emptycell;
 	int	rowi;
 	int	colj;
-	int guess;
-	int safe;
 
 	g_answer = answer;
 	g_size = size;
 	emptycell = findemptycell(board);
-	if(emptycell[0] == -1)
+	if (emptycell[0] == -1)
 	{
-		rowi = 0;
-		while (rowi < g_size)
+		rowi = -1;
+		while (rowi++ < g_size)
 		{
-			colj = 0;
-			while (colj < g_size)
+			colj = -1;
+			while (colj++ < g_size)
 			{
 				g_answer[rowi][colj] = board[rowi][colj];
-				colj++;
 			}
-			rowi++;
 		}
 		free(emptycell);
 		return (1);
 	}
 	rowi = emptycell[0];
 	colj = emptycell[1];
+	return (backagain(board, rowi, colj));
+}
+
+int		backagain(int **board, int row, int col)
+{
+	int guess;
+	int safe;
+
 	guess = 0;
 	while (guess < g_size)
 	{
-		board[rowi][colj] = guess;
-		safe = checkconflict(board, rowi, colj, guess);
+		borad[row][col] = guess;
+		safe = checkconflict(borad, row, col, guess);
 		if (safe == 1)
 		{
 			if (backtracking(board, g_answer, g_size) == 1)
-			{
 				return (1);
-			}
 			else
-			{
-				board[rowi][colj] = 0;
-			}
+				board[row][col] = 0;
 		}
 		else
-		{
-			board[rowi][colj] = 0;
-		}
+			board[row][col] = 0;
+		guess++;
 	}
 	return (-1);
 }
@@ -82,9 +81,9 @@ int		*findemptycell(int **board)
 	while (i < g_size)
 	{
 		j = 0;
-		while(j < g_size)
+		while (j < g_size)
 		{
-			if(board[i][j] == 0)
+			if (board[i][j] == 0)
 			{
 				emptycell[0] = i;
 				emptycell[1] = j;
