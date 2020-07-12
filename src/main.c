45b2672e753ca171cb14d5b802c3e27741f62e0c
Hyeonsik Bae <hybae@c1r8s1.42seoul.kr>
2020-07-12 20:08:54 +0900

    First Edit

--- src/main.c
+++ src/main.c
@@ -6,21 +6,23 @@
 /*   By: hybae <gustlr0217@naver.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/12 14:28:03 by hybae             #+#    #+#             */
-/*   Updated: 2020/07/12 14:42:52 by hybae            ###   ########.fr       */
+/*   Updated: 2020/07/12 20:08:23 by hybae            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include "ft_math.h"
 #include "ft_print.h"
 
+int		g_size;
+
 int		main(int argc, char *argv[])
 {
-	int i;
 	int	**answer;
 	int size;
 
 	size = ft_sqrt(argc - 1);
-	printf("%d",size);
+	g_size = size;
+	answer = solve(argv, g_size);
+	print(answer);
 	return (0);
 }
