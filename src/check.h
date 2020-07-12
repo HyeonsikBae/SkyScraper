eb582d6b5d5b1e853a37e7c963ba8593285f9900
Hyeonsik Bae <hyeonsik12@gmail.com>
2020-07-12 23:06:03 +0900

    Update check.h

--- src/check.h
+++ src/check.h
@@ -1,4 +1,4 @@
-int		checkconflict(int, int, int, int);
+int		checkconflict(int**, int, int, int);
 int		duplicate(int, int, int, int);
 int		move(int, int, int);
 int		moveresult(int, int, int, int, int);
