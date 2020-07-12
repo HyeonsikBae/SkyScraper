1d6764ae20024b7bfb65caae5078d0b17de60835
Hyeonsik Bae <hybae@c1r8s1.42seoul.kr>
2020-07-12 16:02:32 +0900

    Norm Error on solver.c

--- src/solver.c
+++ src/solver.c
@@ -6,11 +6,45 @@
 /*   By: hybae <gustlr0217@naver.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/12 14:46:18 by hybae             #+#    #+#             */
-/*   Updated: 2020/07/12 14:57:29 by hybae            ###   ########.fr       */
+/*   Updated: 2020/07/12 16:01:36 by hybae            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+
+int		g_size;
+int		*top;
+int		*bottom;
+int		*left;
+int		*right;
+int		**initcell(int size);
+void	clueallocate(int *clue);
+
+int		**solve(char *clue, int **answer, int size)
+{
+	int **board;
+	int i;
+
+	g_size = size;
+	top = (int*)malloc(sizeof(int) * g_size);
+	bottom = (int*)malloc(sizeof(int) * g_size);
+	left = (int*)malloc(sizeof(int) * g_size);
+	right = (int*)malloc(sizeof(int) * g_size);
+	**board = initcell(g_size);
+	answer = initcell(g_size);
+	clueallocate(clue);
+	i = 0;
+	while (i < g_size)
+	{
+		backtracking(board);
+		i++;
+	}
+	free(top);
+	free(bottom);
+	free(left);
+	free(right);
+	return (answer);
+}
 
 int		**initcell(int size)
 {
@@ -37,4 +71,19 @@ int		**initcell(int size)
 		i++;
 	}
 	return (puzzle);
+}
+
+void	clueallocate(char *clue)
+{
+	int	i;
+
+	i = 0;
+	while (i < g_size)
+	{
+		top[i] = (int)(clue[i] - '0');
+		bottom[i] = (int)(clue[i + 4] - '0');
+		left[i] = (int)(clue[i + 8] - '0');
+		right[i] = (int)(clue[i + 12] - '0');
+		i++;
+	}
 }
